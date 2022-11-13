cfdb9a47739f16a04cf594f705256bad8eccfb4d
Anhzf <anh.dev7@gmail.com>
2022-11-14 01:55:27 +0700

    Add files via upload

---
+++ _nuxt/ProseH2.e8d5a7a4.js
@@ -0,0 +1,1 @@
+import{f as t,o as s,F as a,G as n,J as r}from"./entry.8616bcda.js";const d=["id"],c=["href"],f=t({__name:"ProseH2",props:{id:null},setup(e){return(o,i)=>(s(),a("h2",{id:e.id},[n("a",{href:`#${e.id}`},[r(o.$slots,"default")],8,c)],8,d))}});export{f as default};
